23280b198b398e544ed426e02cc109a7496e05d5
Justin Sherrill <jsherril@redhat.com>
2019-05-29 09:26:56 -0400

    1.0.3 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-katello',
-    version='1.0.2',
+    version='1.0.3',
     license='GPLv2+',
     packages=find_packages(),
     author='Katello Team',
